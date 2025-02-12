288d881fbd07387beeaad969c09423b2a9fa0dd1
Muhammad-Yuda-Yudistra <yudistira22112022@gmail.com>
2025-02-12 15:48:14 +0700

    chore(analytics) add google analytics at production in vercel
    pa:06-01

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -25,7 +25,18 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
+        <!-- Google tag (gtag.js) -->
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-30774F78SL"></script>
+        <script>
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', 'G-30774F78SL');
+        </script>
+
         <meta name="apple-mobile-web-app-title" content="Amoeba List" />
+        
         <title>{process.env.NEXT_PUBLIC_APP_NAME}</title>
       </head>
       <body
