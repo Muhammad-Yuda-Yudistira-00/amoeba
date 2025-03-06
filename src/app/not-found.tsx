bf0a3f3574269ef3bd55a95a58e735d082c734bb
Muhammad-Yuda-Yudistra <yudistira22112022@gmail.com>
2025-03-06 19:15:00 +0700

    design(home) support mobile responsive with modern design
    pa:20-03b

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -2,7 +2,7 @@ const NotFound = () => {
 	return (
 		<div className="flex flex-col justify-center items-center min-h-screen gap-6 bg-[url('/themes/background/city-1.jpg')] bg-cover">
 	      <div className="text-center">
-	        <h1 className="text-6xl font-mutlu">Page not found</h1>
+	        <h1 className="text-3xl md:text-6xl font-mutlu">Page not found</h1>
 	      </div>
 	    </div>
 	)
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -27,11 +27,11 @@ export default function Home() {
   return (
     <div className="flex flex-col justify-center items-center min-h-screen gap-6 bg-[url('/themes/background/city-1.jpg')] bg-cover">
       <div className="text-center">
-        <h1 className="text-6xl uppercase font-mutlu pb-4">awasome checklist</h1>
+        <h1 className="text-4xl md:text-6xl uppercase font-mutlu pb-4">awasome checklist</h1>
         <small className="font-extralight bg-stone-400 px-2 text-xl">Simple, easy to use, beautiful looks and fun...</small>
       </div>
       <div>
-        <button onClick={handleClick} disabled={loading} className="bg-stone-400 px-4 py-2 border-8 hover:scale-110 hover:px-8 hover:rotate-12 hover:-translate-x-2 uppercase transition-all duration-500">
+        <button onClick={handleClick} disabled={loading} className="bg-stone-400 px-4 py-2 border-8 hover:scale-110 hover:px-8 hover:rotate-12 hover:-translate-x-2 uppercase transition-all duration-500 text-xs md:text-lg">
           {loading ? "Loading.." : "create"}
         </button>
       </div>
