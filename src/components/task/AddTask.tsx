1bf72ad884e99e5ef38b4246c780faf8c9144405
Muhammad-Yuda-Yudistra <yudistira22112022@gmail.com>
2025-03-12 20:13:55 +0700

    design(task) add flash message for add task success
    pa:24-01

--- src/components/task/AddTask.tsx
+++ src/components/task/AddTask.tsx
@@ -6,6 +6,7 @@ import {HttpMethod} from '@/types/HttpMethod'
 export default function AddTask({code, pagination, setTasks, setPagination}: {code: string, pagination: PaginationProps, setTasks: React.Dispatch<React.SetStateAction<Task[]>>, setPagination: React.Dispatch<React.SetStateAction<PaginationProps>>}) {
 	const [task, setTask] = useState<string>("")
 	const [isLoading, setIsLoading] = useState<boolean>(false)
+	const [isSuccess, setIsSuccess] = useState<boolean>(false)
 
 	const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
 		setTask(e.currentTarget.value)
@@ -17,6 +18,7 @@ export default function AddTask({code, pagination, setTasks, setPagination}: {co
 		if(!task.trim()) return
 
 		setIsLoading(true)
+		setIsSuccess(false)
 	
 		const result = await fetchTask({code, method: HttpMethod.POST, contentType: 'application/x-www-form-urlencoded', name: 'title', value: task, currentPage: pagination.currentPage})
 
@@ -29,16 +31,26 @@ export default function AddTask({code, pagination, setTasks, setPagination}: {co
 				}
 				setTask('')
 				setIsLoading(false)
+				setIsSuccess(true)
+				setTiomeout(() => setIsSuccess(false), 3000)
 			}
 		}
 	}
 
 	return (
-			<form onSubmit={handleSubmit} className="flex items-center">
-				<div className="mr-4 w-full">
-					<input type="text" name="task" value={task} onChange={handleChange} placeholder="write your ide.." className="text-amber-700 px-2 rounded-l-2xl pl-4 text-stone-700 focus:outline-amber-400 h-6 w-full" />
+			<form onSubmit={handleSubmit}>
+				<div className="flex items-center bg-yellow-300 px-4 py-2">
+					<div className="mr-4 w-full">
+						<input type="text" name="task" value={task} onChange={handleChange} placeholder="write your ide.." className="text-amber-700 px-2 rounded-l-2xl pl-4 text-stone-700 focus:outline-amber-400 h-6 w-full" />
+					</div>
+					<button className="px-4 py-2 ml-2 text-white bg-amber-700 text-sm hover:text-amber-300 rounded-2xl uppercase disabled:opacity-50 disabled:pointer-events-none" disabled={isLoading}>{isLoading ? 'loading..' : 'add'}</button>
 				</div>
-				<button className="px-4 py-2 ml-2 text-white bg-amber-700 text-sm hover:text-amber-300 rounded-2xl uppercase disabled:opacity-50 disabled:pointer-events-none" disabled={isLoading}>{isLoading ? 'loading..' : 'add'}</button>
+				{isSuccess && (
+					<div className="opacity-75 text-center w-full bg-yellow-400 relative">
+						<div className="absolute blcok -right-0 w-8 bg-stone-400 cursor-pointer hover:bg-black" onClick={() => setIsSuccess(false)}>x</div>
+						<small className="text-lime-700">*success add new task!</small>
+					</div>
+				)}
 			</form>
 		)
 }
