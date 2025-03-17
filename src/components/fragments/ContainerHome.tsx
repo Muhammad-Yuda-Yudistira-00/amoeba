6b6ef989c47d00fd5bec4c9fc06f0986e31f534e
Muhammad-Yuda-Yudistra <yudistira22112022@gmail.com>
2025-03-17 20:52:22 +0700

    chore(home) perubahan yg di amend ternyata tidak terpush tapi dilocal sudah, alternatifnya push uang tanpa berubahan
    pa:27-01a

--- src/components/fragments/ContainerHome.tsx
+++ src/components/fragments/ContainerHome.tsx
@@ -1,4 +1,4 @@
-export default function ContainerHome({children}) {
+export default function ContainerHome({children}: {children: React.ReactNode}) {
 	return(
 		<div className="flex flex-col justify-center items-center min-h-screen gap-14 bg-cover px-2 bg-[radial-gradient(circle,red,#c8dc7d),url('/themes/background/city-1.jpg')] bg-top bg-blend-overlay">
 			{children}
