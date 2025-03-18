69d5596712fc5a85d73816886cdf87ee0c54a222
Muhammad-Yuda-Yudistra <yudistira22112022@gmail.com>
2025-03-18 16:44:20 +0700

    chore(indexing) fix url to appurl, there was typo
    pa:28-02a

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -9,6 +9,6 @@ export default function robots(): MetadataRoute.Robots {
 				allow: '/'
 			}
 		],
-		sitemap: process.env.NEXT_PUBLIC_APP_NAME + '/sitemap.xml'
+		sitemap: process.env.NEXT_PUBLIC_APP_URL + '/sitemap.xml'
 	}
 }
