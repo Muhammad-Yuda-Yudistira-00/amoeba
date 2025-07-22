8cbeaf31291391f318590f1d9287cbd1f2b06aa0
Muhammad-Yuda-Yudistra <yudistira22112022@gmail.com>
2025-07-22 20:40:38 +0700

    chore(env) change .env.local tobe .env.development, change color subtitle roadmap in homepage
    pa:71-0

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -20,7 +20,7 @@ export default function Home() {
     <div className="flex flex-col items-center gap-6 pt-2 pb-4 bg-[url('/themes/background/girls.svg')] bg-cover bg-center w-full h-full">
       <div className="flex flex-col gap-6 md:gap-12 pt-8">
         <div className="text-center md:px-10">
-          <h1 className="text-4xl md:text-5xl font-extrabold dark:text-neutral-800 uppercase bg-white/10">{appName}<small className="text-lg md:text-4xl ms-2 font-semibold text-white dark:text-orange-700 lowercase bg-orange-200/40 px-2"> task manager or todolist</small></h1>
+          <h1 className="text-4xl md:text-5xl font-extrabold dark:text-neutral-800 uppercase bg-white/10">{appName}<small className="text-lg md:text-4xl ms-2 font-semibold text-white dark:text-red-700 lowercase bg-orange-200/40 px-2"> task manager or todolist</small></h1>
           <div className="m-auto w-full pt-14 flex justify-center h-full px-4">
             <Roadmap />
           </div>
